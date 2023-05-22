662ae424aaacc3be7a56785616b5d064b459ed6d
tranha31 <tranquangha31082000@gmail.com>
2023-05-23 00:03:02 +0700

    feat: backup db

---
+++ procedure.sql
@@ -0,0 +1,281 @@
+﻿-- 
+-- Set character set the client will use to send SQL statements to the server
+--
+SET NAMES 'utf8';
+
+--
+-- Set default database
+--
+USE pic_bin_db;
+
+DELIMITER $$
+
+--
+-- Create procedure `Proc_MarketItem_Update`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_MarketItem_Update (IN ImageID varchar(36), IN `Key` varchar(255), IN Caption varchar(500), IN Detail text, IN Price decimal(10, 10), IN Mode int, IN ID varchar(36))
+BEGIN
+
+  IF Mode = 0 THEN
+    INSERT INTO marketitem
+      VALUE (ID, ImageID, `Key`, Caption, Detail, Price, NOW(), NOW());
+  ELSE
+    UPDATE marketitem m
+    SET m.Caption = Caption,
+        m.Detail = Detail,
+        m.Price = Price,
+        m.ModifiedDate = NOW()
+    WHERE m.ID = ID;
+  END IF;
+
+  UPDATE copyrightimage c
+  SET c.Status = 1
+  WHERE c.ImageID = ImageID;
+END
+$$
+
+--
+-- Create procedure `Proc_MarketItem_GetListPagingForHome`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_MarketItem_GetListPagingForHome (IN PublicKey varchar(255), IN FromDate datetime, IN ToDate datetime, IN Start int, IN Length int)
+BEGIN
+  SELECT
+    *
+  FROM marketitem m
+  WHERE m.UserPublicKey <> PublicKey
+  AND (m.CreatedDate BETWEEN FromDate AND ToDate)
+  ORDER BY m.ModifiedDate LIMIT Start, Length;
+END
+$$
+
+--
+-- Create procedure `Proc_MarketItem_GetListPaging`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_MarketItem_GetListPaging (IN PublicKey varchar(255), IN FromDate datetime, IN ToDate datetime, IN Start int, IN Length int)
+BEGIN
+  SELECT
+    *
+  FROM marketitem m
+  WHERE m.UserPublicKey = PublicKey
+  AND (m.CreatedDate BETWEEN FromDate AND ToDate)
+  ORDER BY m.ModifiedDate LIMIT Start, Length;
+END
+$$
+
+--
+-- Create procedure `Proc_MarketItem_Delete`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_MarketItem_Delete (IN ItemID varchar(36))
+BEGIN
+  UPDATE copyrightimage c
+  SET c.Status = 0
+  WHERE c.ImageID = (SELECT
+      m.ImageID
+    FROM marketitem m
+    WHERE m.ID = ItemID);
+
+  DELETE
+    FROM marketitem
+  WHERE ID = ItemID;
+END
+$$
+
+--
+-- Create procedure `Proc_ListImage_Paging`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_ListImage_Paging (IN `Key` varchar(255), IN Start int, IN Length int, IN FromDate datetime, IN ToDate datetime, IN Status int)
+BEGIN
+  IF Status = -1 THEN
+    SELECT
+      *
+    FROM copyrightimage c
+    WHERE c.UserPublicKey = `Key`
+    AND (c.CreatedDate BETWEEN FromDate AND ToDate)
+    ORDER BY c.CreatedDate LIMIT Start, Length;
+  ELSE
+    SELECT
+      *
+    FROM copyrightimage c
+    WHERE c.UserPublicKey = `Key`
+    AND (c.CreatedDate BETWEEN FromDate AND ToDate)
+    AND c.Status = Status
+    ORDER BY c.CreatedDate LIMIT Start, Length;
+  END IF;
+END
+$$
+
+--
+-- Create procedure `Proc_InsertLog`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_InsertLog (IN Message text, IN StackTrace text)
+BEGIN
+  INSERT INTO log
+    VALUE (UUID(), Message, StackTrace, NOW());
+END
+$$
+
+--
+-- Create procedure `Proc_CopyrightImage_Update`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_CopyrightImage_Update (IN ImageID varchar(36), IN NeKey varchar(255), IN OldKey varchar(255), IN SellID varchar(36))
+BEGIN
+  DELETE
+    FROM marketitem
+  WHERE ID = SellID;
+
+  UPDATE copyrightimage c
+  SET c.UserPublicKey = NeKey,
+      c.ModifiedDate = NOW(),
+      c.Status = 0
+  WHERE c.ImageID = ImageID;
+
+  UPDATE copyrightsignature c
+  SET c.NumberImage = c.NumberImage - 1
+  WHERE c.UserPublicKey = OldKey;
+
+  IF EXISTS (SELECT
+        c.RefID
+      FROM copyrightsignature c
+      WHERE c.UserPublicKey = NeKey) THEN
+    UPDATE copyrightsignature c
+    SET c.NumberImage = c.NumberImage + 1
+    WHERE c.UserPublicKey = NeKey;
+  ELSE
+    INSERT INTO copyrightsignature
+      VALUE (UUID(), NeKey, NOW(), 1);
+  END IF;
+
+END
+$$
+
+--
+-- Create procedure `Proc_AuctionRoom_Update`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_AuctionRoom_Update (IN Mode int, IN ImageID varchar(36), IN `Key` varchar(255), IN FromDate datetime, IN ToDate datetime, IN Price decimal(10, 10), IN ItemID varchar(36))
+BEGIN
+  IF Mode = 0 THEN
+    INSERT INTO auctionroom
+      VALUE (ItemID, ImageID, `Key`, FromDate, ToDate, 0, NULL, Price, 0);
+
+    UPDATE copyrightimage c
+    SET c.Status = 2
+    WHERE c.ImageID = ImageID;
+  ELSE
+    UPDATE auctionroom a
+    SET a.StartTime = FromDate,
+        a.EndTime = ToDate,
+        a.StartPrice = Price
+    WHERE a.ID = ItemID;
+  END IF;
+END
+$$
+
+--
+-- Create procedure `Proc_AuctionRoom_Delete`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_AuctionRoom_Delete (IN ItemID varchar(36))
+BEGIN
+  UPDATE copyrightimage c
+  SET c.Status = 0
+  WHERE c.ImageID = (SELECT
+      m.ImageID
+    FROM auctionroom m
+    WHERE m.ID = ItemID);
+
+  DELETE
+    FROM auctionhistory
+  WHERE AuctionRoomID = ItemID;
+
+  DELETE
+    FROM auctionjoin
+  WHERE AuctionRoomID = ItemID;
+
+  DELETE
+    FROM auctionroom
+  WHERE ID = ItemID;
+END
+$$
+
+--
+-- Create procedure `Proc_AuctionRoom_AddHistory`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_AuctionRoom_AddHistory (IN RoomID varchar(36), IN `Key` varchar(255), IN Price decimal(10, 10), IN Action int)
+BEGIN
+  INSERT INTO auctionhistory
+    VALUE (UUID(), RoomID, `Key`, Price, NOW(), Action);
+
+  IF Action = 0 THEN
+    INSERT INTO auctionjoin
+      VALUE (UUID(), RoomID, `Key`);
+
+    SET @HighestPrice = (SELECT
+        SUM(a.Price)
+      FROM auctionhistory a
+      WHERE a.AuctionRoomID = RoomID
+      AND a.UserPublicKey = `Key`
+      AND a.Action = 0);
+
+    UPDATE auctionroom a
+    SET a.HighestPrice = @HighestPrice,
+        a.HighestBeter = `Key`
+    WHERE a.ID = RoomID;
+
+  ELSE
+    DELETE
+      FROM auctionjoin
+    WHERE UserPublicKey = `Key`
+      AND AuctionRoomID = RoomID;
+  END IF;
+END
+$$
+
+--
+-- Create procedure `Proc_AddNew_RequestReject`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_AddNew_RequestReject (IN RefID varchar(36), IN ImageID longtext, IN Reason varchar(255))
+BEGIN
+  INSERT INTO registerreject (RefID, ImageSimilar, ReasonReject)
+    VALUE (RefID, ImageID, Reason);
+END
+$$
+
+--
+-- Create procedure `Proc_AddNewImage`
+--
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE Proc_AddNewImage (IN Sign varchar(45), IN Caption text, OUT ID varchar(36))
+BEGIN
+  SET ID = UUID();
+
+  INSERT INTO copyrightimage
+    VALUE (ID, Sign, Caption, NOW(), NOW(), 0);
+
+  SET @ExistSign = (SELECT
+      1
+    FROM copyrightsignature c
+    WHERE c.UserPublicKey = Sign);
+
+  IF @ExistSign = 1 THEN
+    UPDATE copyrightsignature c
+    SET c.NumberImage = c.NumberImage + 1
+    WHERE c.UserPublicKey = Sign;
+  ELSE
+    INSERT INTO copyrightsignature
+      VALUE (UUID(), Sign, NOW(), 1);
+  END IF;
+
+END
+$$
+
+DELIMITER ;
---
+++ table.sql
@@ -0,0 +1,175 @@
+﻿-- 
+-- Set character set the client will use to send SQL statements to the server
+--
+SET NAMES 'utf8';
+
+--
+-- Set default database
+--
+USE pic_bin_db;
+
+--
+-- Create table `copyrightimage`
+--
+CREATE TABLE copyrightimage (
+  ImageID varchar(36) NOT NULL,
+  UserPublicKey varchar(45) NOT NULL,
+  Caption text DEFAULT NULL,
+  CreatedDate datetime DEFAULT CURRENT_TIMESTAMP,
+  ModifiedDate datetime DEFAULT CURRENT_TIMESTAMP,
+  Status int DEFAULT 0,
+  PRIMARY KEY (ImageID)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 2048,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `marketitem`
+--
+CREATE TABLE marketitem (
+  ID varchar(36) DEFAULT NULL,
+  ImageID varchar(36) DEFAULT NULL,
+  UserPublicKey varchar(255) DEFAULT NULL,
+  Caption varchar(500) DEFAULT NULL,
+  Detail text DEFAULT NULL,
+  Price decimal(10, 10) DEFAULT NULL,
+  CreatedDate datetime DEFAULT CURRENT_TIMESTAMP,
+  ModifiedDate datetime DEFAULT CURRENT_TIMESTAMP
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 8192,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create index `Caption` on table `marketitem`
+--
+ALTER TABLE marketitem
+ADD FULLTEXT INDEX Caption (Caption);
+
+--
+-- Create index `Detail` on table `marketitem`
+--
+ALTER TABLE marketitem
+ADD FULLTEXT INDEX Detail (Detail);
+
+--
+-- Create foreign key
+--
+ALTER TABLE marketitem
+ADD CONSTRAINT FK_marketitem_copyrightimage FOREIGN KEY (ImageID)
+REFERENCES copyrightimage (ImageID);
+
+--
+-- Create table `auctionroom`
+--
+CREATE TABLE auctionroom (
+  ID varchar(36) NOT NULL DEFAULT '',
+  ImageID varchar(36) NOT NULL DEFAULT '',
+  OwnerPublicKey varchar(255) DEFAULT NULL,
+  StartTime datetime DEFAULT NULL,
+  EndTime datetime DEFAULT NULL,
+  HighestPrice decimal(10, 10) DEFAULT 0.0000000000,
+  HighestBeter varchar(255) DEFAULT NULL,
+  StartPrice decimal(10, 10) DEFAULT 0.0000000000,
+  Status int DEFAULT 0,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 16384,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create foreign key
+--
+ALTER TABLE auctionroom
+ADD CONSTRAINT FK_auctionroom_ImageID FOREIGN KEY (ImageID)
+REFERENCES copyrightimage (ImageID);
+
+--
+-- Create table `auctionjoin`
+--
+CREATE TABLE auctionjoin (
+  ID varchar(36) NOT NULL DEFAULT '',
+  AuctionRoomID varchar(36) DEFAULT NULL,
+  UserPublicKey varchar(255) DEFAULT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create foreign key
+--
+ALTER TABLE auctionjoin
+ADD CONSTRAINT FK_auctionjoin_AuctionRoomID FOREIGN KEY (AuctionRoomID)
+REFERENCES auctionroom (ID);
+
+--
+-- Create table `auctionhistory`
+--
+CREATE TABLE auctionhistory (
+  ID varchar(36) NOT NULL DEFAULT '',
+  AuctionRoomID varchar(36) DEFAULT NULL,
+  UserPublicKey varchar(255) DEFAULT NULL,
+  Price decimal(10, 10) DEFAULT 0.0000000000,
+  CreatedTime datetime DEFAULT NULL,
+  Action int DEFAULT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create foreign key
+--
+ALTER TABLE auctionhistory
+ADD CONSTRAINT FK_auctiondetail_AuctionRoomID FOREIGN KEY (AuctionRoomID)
+REFERENCES auctionroom (ID);
+
+--
+-- Create table `registerreject`
+--
+CREATE TABLE registerreject (
+  RefID varchar(36) DEFAULT NULL,
+  ImageSimilar longtext DEFAULT NULL,
+  ReasonReject varchar(255) DEFAULT NULL
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 8192,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `log`
+--
+CREATE TABLE log (
+  RefID varchar(36) DEFAULT NULL,
+  Message text DEFAULT NULL,
+  StackTrace longtext DEFAULT NULL,
+  CreatedDate datetime DEFAULT NULL
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 2340,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
+
+--
+-- Create table `copyrightsignature`
+--
+CREATE TABLE copyrightsignature (
+  RefID varchar(36) NOT NULL,
+  UserPublicKey varchar(45) NOT NULL,
+  CreatedDate datetime DEFAULT NULL,
+  NumberImage int NOT NULL,
+  PRIMARY KEY (RefID)
+)
+ENGINE = INNODB,
+AVG_ROW_LENGTH = 5461,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci;
